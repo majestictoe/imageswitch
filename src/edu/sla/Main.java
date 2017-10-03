9f9d87ac67f7c3d4b6fa4736f5b46a794a1d3303
majestictoe <eshamble18@scienceleadership.org>
2017-10-03 10:21:10 -0400

    it works now

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -1,27 +1,32 @@
 package edu.sla;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
 
 public class Main extends Application {
 
+
     public void start(Stage primaryStage) throws Exception {
-        Label hi = new Label("bees");
-        Scene scene1 = new Scene (hi,400, 400);
+
+        Button pictureButton = new Button();
+        Button pictureButton2 = new Button();
+        Image knifecat = new Image("images.jpeg",400,400,false,true);
+        Image sadcat = new Image ("p0a19.jpg",400,400,false,true);
+        pictureButton.setGraphic(new ImageView(sadcat));
+        pictureButton2.setGraphic(new ImageView (knifecat));
+        Scene scene1 = new Scene (pictureButton,400, 400);
+        Scene scene2 = new Scene (pictureButton2,400, 400);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("hgehvj");
+        pictureButton.setOnAction((ActionEvent e) -> primaryStage.setScene(scene2));
+        pictureButton2.setOnAction((ActionEvent e) -> primaryStage.setScene(scene1));
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         Application.launch(args);
     }
