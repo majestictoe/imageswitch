e609917cf5f830741d35d22b494d489b40e31f99
majestictoe <eshamble18@scienceleadership.org>
2017-10-03 10:28:31 -0400

    added all the images onto buttons. it works now

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -15,16 +15,21 @@ public class Main extends Application {
 
         Button pictureButton = new Button();
         Button pictureButton2 = new Button();
+        Button pictureButton3 = new Button();
         Image knifecat = new Image("images.jpeg",400,400,false,true);
         Image sadcat = new Image ("p0a19.jpg",400,400,false,true);
+        Image screamcat = new Image ("just-a-cat-falling-off-a-table-227148.jpg",400,400,false,true);
         pictureButton.setGraphic(new ImageView(sadcat));
         pictureButton2.setGraphic(new ImageView (knifecat));
+        pictureButton3.setGraphic(new ImageView (screamcat));
         Scene scene1 = new Scene (pictureButton,400, 400);
         Scene scene2 = new Scene (pictureButton2,400, 400);
+        Scene scene3 = new Scene (pictureButton3,400,400);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("hgehvj");
         pictureButton.setOnAction((ActionEvent e) -> primaryStage.setScene(scene2));
-        pictureButton2.setOnAction((ActionEvent e) -> primaryStage.setScene(scene1));
+        pictureButton2.setOnAction((ActionEvent e) -> primaryStage.setScene(scene3));
+        pictureButton3.setOnAction((ActionEvent e) -> primaryStage.setScene(scene1));
         primaryStage.show();
     }
     public static void main(String[] args) {
